f00b6e53460bfabaa1f971fae591533253d4deb3
Brendon Plummer <brendon.plummer99@gmail.com>
2023-11-20 19:14:58 +0000

    Ammendments to comments for added description

--- client.py
+++ client.py
@@ -4,20 +4,24 @@ import threading
 # Server configuration
 HOST = '127.0.0.1'
 PORT = 5555
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a socket
 client_socket.connect((HOST, PORT)) # Connect to the server
 
+
+#For some reason messaging order seems weird, comes out as expected but doesnt format correctly
 # Function to send messages to the server
 def send_message():
     while True:
-        message = input("You: ")
+        message = input("\nYou: ") #The input doesnt quite work properly, maybe due to same machine use? Try on seperate machine
         client_socket.send(message.encode())
 
+
+#Clients still recieve and send messages as expected, recieve seems to not display quite as expected
 # Function to receive messages from the server
 def receive_message():
     while True:
         try:
-            data = client_socket.recv(1024) # Receive data from the server
+            data = client_socket.recv(1024) # Receives data from the server
             if not data:
                 break
             print("\nOther: " + data.decode()) # Display the received message
@@ -26,7 +30,7 @@ def receive_message():
             print(f"Error: {e}")
             break
 
-# Start threads for sending and receiving messages
+# Start seperate threads for sending and receiving messages
 send_thread = threading.Thread(target=send_message)
 receive_thread = threading.Thread(target=receive_message)
 send_thread.start()
--- server.py
+++ server.py
@@ -4,12 +4,13 @@ import threading
 # Server configuration
 HOST = '127.0.0.1'
 PORT = 5555
-# Create a socket 
+
+# Creates a socket 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT)) # Bind the socket to a specific address and port 
+server_socket.bind((HOST, PORT)) # Binds the socket to a specific address and port, set to const values HOST & PORT
 server_socket.listen() # Listen for incoming connections
 print(f"Server listening on {HOST}:{PORT}")
-clients = [] # List to store client sockets
+clients = [] # List to store client sockets, allows for multiple connections, a limit could be implemented if resources are scarce
 
 # Function to handle client connections
 def handle_client(client_socket):
@@ -22,15 +23,15 @@ def handle_client(client_socket):
             # Broadcast the received message to all clients
             for client in clients:
                 client.send(data)
+                
         except Exception as e:
             print(f"Error: {e}")
             break
 
 # Main server loop
 while True:
-    # Accept a new connection
+    # Accept a new connection, adds to client list
     client_socket, client_address = server_socket.accept()
-    # Add the new client to the list
     clients.append(client_socket)
     # Start a new thread to handle the client
     client_handler = threading.Thread(target=handle_client, args=(client_socket,))
